c0bfc082d25e75a20e61c83aa8223a7e9ac2dd09
rabinacharya11 <charms2074@gmail.com>
2024-03-27 01:26:29 +0545

    search done

---
+++ lib/core/helpers/list_unique_checker.dart
@@ -0,0 +1,13 @@
+class ListUniqueChecker {
+
+ static bool isUnique<T>(List<T> list, T element) {
+    for (var existingElement in list) {
+      if (existingElement == element) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+
